6f3a0d80e5e353cf715e5c1b4b318bc1130db42a
Jeong Jihyo <jihyo0331@gmail.com>
2024-08-06 23:32:25 +0900

    feat: 트리 늘어나는 기능 추가

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-//script.js
-const bground = document.querySelector(".Bground")
+// Snowfall effect
+const bground = document.querySelector(".Bground");
 
-for(let i = 0; i < 200; i++){
-    bground.insertAdjacentHTML("afterbegin",'<div class="snow"></div>')
+for (let i = 0; i < 200; i++) {
+    bground.insertAdjacentHTML("afterbegin", '<div class="snow"></div>');
 }
 
 document.getElementById('addLayerButton').addEventListener('click', function() {
-    // 새로운 M.png 요소를 생성합니다.
+    // 새로운 중간트리 생성
     const newLayer = document.createElement('img');
     newLayer.src = 'img/M.png';
     newLayer.className = 'M';
-
-    // 마지막 M 요소의 아래에 추가합니다.
     const treeContainer = document.getElementById('treeContainer');
-    const lastM = treeContainer.querySelectorAll('.M');
-    const lastMElement = lastM[lastM.length - 1];
-    const newTop = parseFloat(lastMElement.style.top) + 5; // Adjust the value as needed
+    const topTree = document.getElementById('topTree');
 
-    newLayer.style.top = `${newTop}%`;
+    // 기존 중간트리 레이어의 위치 가져옴
+    const allMLayers = treeContainer.querySelectorAll('.M');
+    const firstMLayer = allMLayers[0]; // 첫 번째 M 레이어
+    const firstMTop = parseFloat(window.getComputedStyle(firstMLayer).top);
 
-    treeContainer.insertBefore(newLayer, treeContainer.querySelector('.B'));
+    // 새로운 레이어의 위치를 첫 번째 중간트리 레이어 위로 설정
+    newLayer.style.top = `${firstMTop - 90}px`;
+
+    // 새로운 레이어를 첫 번째 중간트리 레이어 앞에 추가
+    treeContainer.insertBefore(newLayer, firstMLayer);
+
+    // 위 트리 이미지 이동
+    const latestM = treeContainer.querySelectorAll('.M')[0];
+    topTree.style.top = `${parseFloat(latestM.style.top) - 140}px`;
 });
-
-
